87422d7b719c12c502c47a0ac8c944abfa40549d
Nishu <nimish.prabhakar@eagleview.com>
2021-03-27 16:15:45 +0530

    new code added

--- posts/index.js
+++ posts/index.js
@@ -23,20 +23,20 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  // await axios.post("http://localhost:4005/events", {
-  //   type: 'PostCreated',
-  //   data: {
-  //     id,
-  //     title
-  //   }
-  // })
+  await axios.post("http://localhost:4005/events", {
+    type: 'PostCreated',
+    data: {
+      id,
+      title
+    }
+  })
 
   res.status(201).send(posts[id]);
 });
 
-// app.post('/events', (req, res) => {
-//   console.log('recieved post event',req.body.type);
-// })
+app.post('/events', (req, res) => {
+  console.log('recieved post event',req.body.type);
+})
 
 app.listen(4000, () => {
   console.log('Listening on 4000');
