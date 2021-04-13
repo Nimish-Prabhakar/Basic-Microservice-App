8905aac17aee40607b8d52f2f5d6646191137b18
Nishu <nimish.prabhakar@eagleview.com>
2021-04-13 16:26:06 +0530

    removed dependency for query service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    if (comment.status === 'pending') {
+      content = 'The comment is undergoing moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'The comment was rejected';
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
